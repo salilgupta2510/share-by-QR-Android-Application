acf001b3209e65cbd903e2572cc2b7aeee29b6f7
Eshun Sharma <eshun.sharma@harman.com>
2017-10-31 20:48:48 +0530

    Linting Changes

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import BarcodeScannerExample from './Component/scanner.js';
 import FirstPage from './Component/menu.js';
 
 export default class App extends React.Component {
   render() {
-    return (
-      
-        <FirstPage />
-      
-    );
+    return (<FirstPage />);
   }
 }
-
-
